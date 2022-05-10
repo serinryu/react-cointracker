225c21da4baaf3fc508b39a78f794794c92507ff
serinryu <serin0404@naver.com>
2022-05-10 12:33:25 +0900

    useParams

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,4 +1,7 @@
+import { useParams } from "react-router";
+
 function Coin() {
-  return <h1>Coin</h1>;
+  const { coinId } = useParams();
+  return <h1>Coin: {coinId}</h1>;
 }
 export default Coin;
