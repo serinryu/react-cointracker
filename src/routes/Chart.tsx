ef50700c77c343a5d200560361f349bb2c28016f
serinryu <serin0404@naver.com>
2022-05-15 02:04:35 +0900

    Price Chart (2)

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -6,11 +6,23 @@ interface ChartProps {
   coinId: string;
 }
 
+interface IHistorical {
+  time_open: string;
+  time_close: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
+}
+
 function Chart() {
   const { coinId } = useOutletContext<ChartProps>();
-  const { isLoading, data } = useQuery<ChartProps>(["ohlcv", coinId], () => fetchCoinHistory(coinId));
+  const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () => fetchCoinHistory(coinId));
   return (
     <>
+    {data?.map((price) => price.close)}
     </>
   );
 }
