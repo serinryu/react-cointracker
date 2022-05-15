ff223414b989c3dbe01dfe30680385ea37d39db1
serinryu <serin0404@naver.com>
2022-05-16 02:35:10 +0900

    ApexChart

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,6 +1,7 @@
 import { useOutletContext } from 'react-router-dom';
 import { useQuery } from "react-query";
 import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts";
 
 interface ChartProps {
   coinId: string;
@@ -22,7 +23,58 @@ function Chart() {
   const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () => fetchCoinHistory(coinId));
   return (
     <>
-    {data?.map((price) => price.close)}
+       {isLoading ? (
+        "Loading chart..."
+      ) : (
+        <ApexChart
+          type="line"
+          series={[
+            {
+              name: "Price",
+              data: data?.map((price) => price.close)!, //! means never null
+              //data?.map((price => price.close)) ?? []  also works (consider undefined)
+            },
+          ]}
+          options={{
+            theme: {
+              mode: "dark",
+            },
+            chart: {
+              height: 300,
+              width: 500,
+              toolbar: {
+                show: false,
+              },
+              background: "transparent",
+            },
+            grid: { show: false },
+            stroke: {
+              curve: "smooth",
+              width: 4,
+            },
+            yaxis: {
+              decimalsInFloat: 0,
+            },
+            xaxis: {
+              axisBorder: { show: true },
+              axisTicks: { show: true },
+              labels: { show: true },
+              type: "datetime",
+              categories: data?.map((price) => price.time_close),
+            },
+            fill: {
+              type: "gradient",
+              gradient: { gradientToColors: ["#0be881"], stops: [0, 100] },
+            },
+            colors: ["#0fbcf9"],
+            tooltip: {
+              y: {
+                formatter: (value) => `$${value.toFixed(2)}`,
+              },
+            },
+          }}
+      />
+    )} 
     </>
   );
 }
